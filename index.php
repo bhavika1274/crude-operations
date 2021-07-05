265fe2d7372a83695ae03513680b8731afa71c63
bhavika <bhavika1274@gmail.com>
2021-07-05 23:16:26 +0530

    seccommit

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
    <link rel="stylesheet" href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
  
-   <title>Crude Operations</title>
+   <title>Crude  with Operations</title>
  </head>
  <body>
     <!-- Button trigger modal 
